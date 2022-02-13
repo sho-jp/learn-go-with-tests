30e30e47b08ca86de108259d4c954d0ffb5c90ba
sho171 <sh.zxcv00@gmail.com>
2022-02-13 23:32:30 +0900

    modify

--- slice/slice.go
+++ slice/slice.go
@@ -9,9 +9,22 @@ func Sum(numbers []int) (sum int) {
 }
 
 func SumAll(numbersToSum ...[]int) (sums []int) {
-	for _, v := range numbersToSum {
-		sums = append(sums, Sum(v))
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
 	}
 
 	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return
 }
